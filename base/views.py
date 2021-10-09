8c40cd34536ff5e60c1ef09b6f8e1aed87fd6070
bekzod818 <bekzod030900@gmail.com>
2021-10-09 21:40:35 +0500

    Search

--- base/views.py
+++ base/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__contains=q)
+    )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
     context = {
         'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
     }
     return render(request, 'base/home.html', context)
 
